392813618d40993243b577ce11272499a50d3296
AmmarMohyuddin <ammar.mohyuddin45@gmail.com>
2025-04-29 03:38:12 +0500

    customer lisitng api updated

--- controllers/api/v1/customers/list.js
+++ controllers/api/v1/customers/list.js
@@ -5,9 +5,59 @@ const {
 } = require("../../../../utils/response");
 
 async function list(req, res) {
+  const { customer_name, account_number } = req.query;
+
   try {
-    const customers = await Customer.find();
-    if (!customers || customers.length === 0) {
+    if (customer_name && account_number) {
+      const customers = await Customer.find({
+        party_name: customer_name,
+        account_number: account_number,
+      });
+
+      const addressLines = [
+        ...new Set(customers.map((customer) => customer.address_line_1)),
+      ];
+
+      return successResponse(
+        res,
+        200,
+        "Unique address lines retrieved successfully.",
+        addressLines
+      );
+    }
+
+    if (customer_name && !account_number) {
+      const customers = await Customer.find({ party_name: customer_name });
+      const accountNumbers = [
+        ...new Set(customers.map((customer) => customer.account_number)),
+      ];
+      if (!customers || customers.length === 0) {
+        return errorResponse(res, 404, "Customer not found.");
+      }
+
+      return successResponse(
+        res,
+        200,
+        "Account Numbers retrieved successfully.",
+        accountNumbers
+      );
+    }
+
+    const uniqueCustomers = await Customer.aggregate([
+      {
+        $group: {
+          _id: "$party_name",
+          customer: { $first: "$$ROOT" },
+        },
+      },
+      {
+        $replaceRoot: { newRoot: "$customer" },
+      },
+    ]);
+
+    console.log("Unique customers:", uniqueCustomers);
+
+    if (!uniqueCustomers || uniqueCustomers.length === 0) {
       return successResponse(
         res,
         200,
@@ -15,11 +65,12 @@ async function list(req, res) {
         []
       );
     }
+
     return successResponse(
       res,
       200,
       "Customers retrieved successfully.",
-      customers
+      uniqueCustomers
     );
   } catch (error) {
     console.error("Error retrieving customers:", error.stack);
